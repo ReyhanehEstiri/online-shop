85be319e1f381587c95c900d5aeb624272a72f8a
Matin Ghanbari <ghanbarimatin6@gmail.com>
2023-12-31 23:53:56 +0330

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import { BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 import Navbar from "./components/navbar";
-import Profile from "./pages/profile";
 import BookstoreRoutes from './components/BookstoreRoute';
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    // <div className="App">
-      
-      <BrowserRouter>
-            <BookstoreRoutes />
-            <Navbar />
-        </BrowserRouter>
-
-    // </div>
+    <BrowserRouter>
+          <BookstoreRoutes />
+          <Navbar />
+      </BrowserRouter>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,43 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from "react-router-dom"
 import '../cssfile/navbar.css';
 
 const Navbar = () => {
   const [userCart, setUserCart] = useState([]);
+  const [isLoading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  const getUserBooks = ()=>{
-    try {
-      var user_id = localStorage.getItem('user_id');
-      fetch('https://localhost:7268/api/v1/Book/GetUserCart?userId='+user_id, {
-        method: 'GET',
-        headers: {
-          'ngrok-skip-browser-warning':true,
-          'accept': '*/*', 
-        },
-      })
-        .then((response) => {
-          if (!response.ok) {
-            return "";
-          }
-          return response.json();
+
+  useEffect(() => {
+    const getUserBooks = ()=>{
+      try {
+        var user_id = localStorage.getItem('user_id');
+        fetch('https://localhost:7268/api/v1/Book/GetUserCart?userId='+user_id, {
+          method: 'GET',
+          headers: {
+            'ngrok-skip-browser-warning':true,
+            'accept': '*/*', 
+          },
         })
-        .then((data) => {
-          setUserCart(data);
-        });
-      } catch (error) {
-        return "";
-      } finally {
-      }
+          .then((response) => {
+            if (!response.ok) {
+              return "";
+            }
+            return response.json();
+          })
+          .then((data) => {
+            setUserCart(data);
+          });
+        } catch (error) {
+          return "";
+        } finally {
+          setLoading(false);
+        }
 
       if(userCart)
         return "("+userCart.length+")";
       return "";
-  };
+    };
 
-  const GetCart = ()=>{
-    
-  }
+    if(isLoading)
+      getUserBooks();
+  });
 
   const handleClick = () => {
     navigate('/basket');
@@ -55,12 +59,15 @@ function navigateToHome(){
         //  onClick={navigateToHome()}
          >Online Book Shop</div>
         <ul>
-          <li><Link to="/profile#1">Wishlist</Link></li>
-          <li><Link to="/profile">My profile</Link></li>
+          {/* <li><Link to="/profile#1">Wishlist</Link></li>   */}
           <li><Link to="/">Home</Link></li>
+          <li><Link to="/profile">My profile</Link></li>
+          <li><Link to="/Login">Login</Link></li>
+          <li><Link to="/SignUp">SignUp</Link></li>
+          {/* <li><Link to="/"></Link></li> */}
         </ul>
           <button className="cart-button" onClick={(handleClick)}>
-              Cart {getUserBooks()}
+              Cart {userCart.length}
           </button>
       </div>
     </nav>
